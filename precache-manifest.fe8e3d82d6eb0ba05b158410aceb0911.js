216f439a63c3cdb382e924271191c4de44b7a39e
sahinreja <sahinreja512@gmail.com>
2020-09-09 01:27:45 +0530

    Updates

---
+++ precache-manifest.fe8e3d82d6eb0ba05b158410aceb0911.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7eb421a859590e73490f79a85992ec",
+    "url": "/everythingaos/index.html"
+  },
+  {
+    "revision": "8d39d1131e4aff3bc5fe",
+    "url": "/everythingaos/static/css/2.df750b2b.chunk.css"
+  },
+  {
+    "revision": "ff29c0a4d1aca66f040e",
+    "url": "/everythingaos/static/css/main.02beb2c3.chunk.css"
+  },
+  {
+    "revision": "8d39d1131e4aff3bc5fe",
+    "url": "/everythingaos/static/js/2.aa120d4b.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/everythingaos/static/js/2.aa120d4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff29c0a4d1aca66f040e",
+    "url": "/everythingaos/static/js/main.b033532c.chunk.js"
+  },
+  {
+    "revision": "255735e1d86f13f0a626",
+    "url": "/everythingaos/static/js/runtime-main.e9c1770d.js"
+  },
+  {
+    "revision": "2b2ad1d1934015431923555f343d6bf8",
+    "url": "/everythingaos/static/media/banner.2b2ad1d1.svg"
+  },
+  {
+    "revision": "f9758f16dc7b16f39489d0399e9115ef",
+    "url": "/everythingaos/static/media/boots.f9758f16.png"
+  },
+  {
+    "revision": "ed08e01322359df784b0d2d80e2ee1a2",
+    "url": "/everythingaos/static/media/html.ed08e013.gif"
+  }
+]);
